e5028f53a34b37122d46fcc68f1c61ff4ffff464
Jason Zhou <104427@US-CompLabiMac-02.local>
2025-09-22 09:35:34 -0400

    Day_Efficacy.java 09/22 v1

---
+++ Day_Efficacy.java
@@ -0,0 +1,12 @@
+public class Day_Efficacy {
+	
+	
+	double price;
+	double weather;
+	
+	public double calc_Weather() {
+	double[] weathers = {0.70, 0.75, 0.80, 0.85. 0.90, 0.95, 1.00};
+	String[] Weather_names = {"Cloudy 70 degrees", "Cloudy 75 degrees," "Hazy 80 degrees," "Cloudy 85 degrees," "Sunny 60 degrees," "Sunny 75 degrees"};
+	
+	}
+}
