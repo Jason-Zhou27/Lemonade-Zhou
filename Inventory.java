2a113dadc425bd8409a2ac694712ad3d35e6d55a
Jason Zhou <104427@US-CompLabiMac-02.local>
2025-09-22 09:03:49 -0400

    Main.java 09/22 v2

---
+++ Inventory.java
@@ -0,0 +1,29 @@
+public class Inventory {
+	double money = 100.00;
+	double sugarPrice = 0.15;
+	double lemonPrice = 0.20;
+	double icePrice = 0.05;
+	double cupPrice = 0.03;
+	int lemons;
+	int sugar;
+	int ice;
+	int cups;
+		
+	Scanner sc = new Scanner(system.in);
+	System.out.print("You have " + money + "dollars");
+	System.out.print("Enter # of lemons: ");
+	int buyLemons = Integer.parseInt(sc.nextLine());
+	System.out.print("Enter # of sugar: ");
+	int buySugar = Integer.parseInt(sc.nextLine());
+	System.out.print("Enter # of ice:");
+	int buyIce = Integer.parseInt(sc.nextLine());
+	System.out.print("Enter # of cups: ");
+	int buyCups = Integer.parseInt(sc.nextLine());
+		
+	public void updateInventory() {
+		lemons+=buyLemons;
+		sugar+=buySugar;
+		ice+=buyIce;
+		cup+=buyCups;
+	}
+}
--- Main.java
+++ Main.java
@@ -4,31 +4,4 @@ public class Main {
 	Lemonade l1 = new LemonadeStand();
 		l1.start();
 	}
-	public class Inventory {
-		double money = 100.00;
-		double sugarPrice = 0.15;
-		double lemonPrice = 0.20
-		double icePrice = 0.05;
-		double cupPrice = 0.03;
-		int lemons;
-		int sugar;
-		int ice;
-		int cups;
-		
-		Scanner sc = new Scanner(system.in);
-		System.out.println("You have " + money "dollars");
-		System.out.print("Enter # of lemons");
-		int buyLemons = Integer.parseInt(sc.nextLine());
-		System.out.print("Enter # of sugar ");
-		int buySugar = Integer.parseInt(sc.nextLine());
-		System.out.print("Enter # of ice");
-		int buyIce = Integer.parseInt(sc.nextLine());
-		System.out.print("Enter # of cups ");
-		int buyCups = Integer.parseInt(sc.nextLine());
-		
-		public void updateInventory {
-			lemons+=buyLemons;
-			sugar+=buySugar;
-			ice+=buyIce;
-			cup+=buyCups;
 }
