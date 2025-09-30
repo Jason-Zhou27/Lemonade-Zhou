7e9613b6cf06d4a72b8a1cffa859205bda0ba77b
Jason-Zhou27 <zhoujas27@7hills.org>
2025-09-30 18:58:23 -0400

    hw for October 2nd commit 3

--- Lemonade_Stand.java
+++ Lemonade_Stand.java
@@ -5,13 +5,54 @@
 public class Lemonade_Stand {
 	
 	public Lemonade_Stand(){
+	//defaults to a 7 day game
 		
 	Inventory i1 = new Inventory();
 	Choice c1 = new Choice();
 	Day_Efficacy de1 = new Day_Efficacy();
 	Day_Results dr1 = new Day_Results();
 	
+	Inventory i2 = new Inventory();
+	Choice c2 = new Choice();
+	Day_Efficacy de2 = new Day_Efficacy();
+	Day_Results dr2 = new Day_Results();
+	
+	Inventory i3 = new Inventory();
+	Choice c3 = new Choice();
+	Day_Efficacy de3 = new Day_Efficacy();
+	Day_Results dr3 = new Day_Results();
+	
+	Inventory i4 = new Inventory();
+	Choice c4 = new Choice();
+	Day_Efficacy de4 = new Day_Efficacy();
+	Day_Results dr4 = new Day_Results();
+	
+	Inventory i5 = new Inventory();
+	Choice c5 = new Choice();
+	Day_Efficacy de5 = new Day_Efficacy();
+	Day_Results dr5 = new Day_Results();
+	
+	Inventory i6 = new Inventory();
+	Choice c6 = new Choice();
+	Day_Efficacy de6 = new Day_Efficacy();
+	Day_Results dr6 = new Day_Results();
+	
+	Inventory i7 = new Inventory();
+	Choice c7 = new Choice();
+	Day_Efficacy de7 = new Day_Efficacy();
+	Day_Results dr7 = new Day_Results();
+	
+	endGame();
+	
 	
 	}
+	public void endGame(){
+		System.out.println("Game Over");
+		finalResults();
+		return 0;
+	}
+	public void finalResults(){
+		
+	}	
 
 }
